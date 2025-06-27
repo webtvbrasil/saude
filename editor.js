b2cb2014e884a6011b73122f2a067696ec714ede
webtvbrasil <webtvbrasil.br@gmail.com>
2025-06-27 04:33:52 -0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -1,14 +1,22 @@
 // script.js
 
+
 document.addEventListener('DOMContentLoaded', function() {
     // ID do Vídeo do YouTube
-    const videoId = 'NOVO_ID_DO_VIDEO'; // Substitua pelo novo ID do vídeo
+    const videoId = 'qYC2IBnF48s'; // Substitua pelo novo ID do vídeo
+
+    const titulo = '0000000';
 
     // Link do Botão de Checkout
-    const checkoutLink = 'https://SEU_LINK_DE_CHECKOUT'; // Substitua pelo novo link de checkout
+    const checkoutLink = 'https://pay.kiwify.com.br/4K0A9aJ'; // Substitua pelo novo link de checkout
 
     // Link do WhatsApp de Suporte
-    const whatsappLink = 'https://wa.me/SEU_NUMERO_WHATSAPP'; // Substitua pelo novo número de WhatsApp
+    const whatsappLink = 'https://wa.me/5547992691983'; // Substitua pelo novo número de WhatsApp
+
+    const titulo = document.querySelector('.headline');
+    if (titulo) {
+        titulo.textContent = `Reverta sua <span class="orange-text">Diabetes Tipo 2</span> de forma natural!`;
+    }
 
     // 1. Alterar o ID do Vídeo
     const videoIframe = document.querySelector('.video-container iframe');
